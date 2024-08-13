d01522b512ed7f443c3f32738b9aab3e6e5e9536
18777293000 <120953990@qq.com>
2024-08-13 14:56:26 +0800

    调试飞花令轮播图高度

--- src/renderer/screens/game/FeihuaScreen.vue
+++ src/renderer/screens/game/FeihuaScreen.vue
@@ -9,13 +9,7 @@
           </v-col>
           <!-- style="z-index: 12;" 加上这个让幻灯片悬在排名之上 -->
           <v-col cols="12">
-            <v-carousel
-              v-model="currentPage"
-              :show-arrows="false"
-              height="300"
-              hide-delimiters
-              class=""
-            >
+            <v-carousel v-model="currentPage" show-arrows="hover" :height="height" hide-delimiters class="border-lg">
               <v-carousel-item value="0" class="">
                 <div class="pa-6">
                   <div class="d-flex justify-center align-center error-border-inner">
@@ -26,17 +20,11 @@
               </v-carousel-item>
               <v-carousel-item value="1" class="">
                 <div class="d-flex flex-column justify-center mb-6">
-                  <div class="feihua-subtitle-1 ma-2 pa-2"
-                    >系统给出一个关键字, 玩家要在90秒内想出并输入一句含有该字的诗句。</div
-                  >
-                  <div class="feihua-subtitle-1 ma-2 pa-2"
-                    >如: 问题:请写出含有<span style="color: #00c853; font-size: 2rem">"春"</span
-                    >的诗句：</div
-                  >
-                  <div class="feihua-subtitle-1 ma-2 pa-2"
-                    >则回答：<span style="color: #00c853; font-size: 2rem"
-                      ><u>春城无处不飞花</u></span
-                    >，即算正确，跳转到下个问题。
+                  <div class="feihua-subtitle-1 ma-2 pa-2">系统给出一个关键字, 玩家要在90秒内想出并输入一句含有该字的诗句。</div>
+                  <div class="feihua-subtitle-1 ma-2 pa-2">如: 问题:请写出含有<span
+                      style="color: #00c853; font-size: 2rem">"春"</span>的诗句：</div>
+                  <div class="feihua-subtitle-1 ma-2 pa-2">则回答：<span
+                      style="color: #00c853; font-size: 2rem"><u>春城无处不飞花</u></span>，即算正确，跳转到下个问题。
                   </div>
                 </div>
               </v-carousel-item>
@@ -44,9 +32,9 @@
                 <div class="pa-6 success-border">
                   <div class="pa-6 success-border-inner">
                     <!-- <h1 style="color: #2E7D32;">回答正确</h1><br /> -->
-                    <h2>《{{ completeShici.title }}》</h2>
+                    <h2 class="feihua-h4">《{{ completeShici.title }}》</h2>
                     <h3>{{ completeShici.author }}</h3>
-                    <p style="white-space: pre-line; margin-top: 4px">{{
+                    <p class="feihua-h5" style="white-space: pre-line; margin-top: 4px">{{
                       addBreaksToPoem(completeShici.content)
                     }}</p>
                     <!-- <p v-for="(item, index) in slicePoem(completeShici.content)" :key="index">{{ item }}</p> -->
@@ -60,36 +48,15 @@
       <v-col cols="12" class="">
         <v-row align="center" justify="start" class="text-center">
           <v-col cols="3" class="">
-            <v-progress-circular
-              :color="colors[alertColor]"
-              :model-value="time"
-              :size="200"
-              :width="10"
-            >
-              <v-card
-                v-show="!play"
-                class="mx-auto rounded-circle bg-amber-lighten-4"
-                height="140"
-                width="140"
-              >
-                <div
-                  class="d-flex fill-height align-center justify-center feihua-h3 circle-background"
-                  style="color: #1b5e20"
-                  @click="gamePlay"
-                  >{{ startBtn }}</div
-                >
+            <v-progress-circular :color="colors[alertColor]" :model-value="time" :size="200" :width="10">
+              <v-card v-show="!play" class="mx-auto rounded-circle bg-amber-lighten-4" height="140" width="140">
+                <div class="d-flex fill-height align-center justify-center feihua-h3 circle-background"
+                  style="color: #1b5e20" @click="gamePlay">{{ startBtn }}</div>
               </v-card>
               <v-expand-x-transition>
-                <v-card
-                  v-show="play"
-                  class="mx-auto rounded-circle bg-amber-lighten-4"
-                  height="140"
-                  width="140"
-                >
-                  <div
-                    class="d-flex fill-height align-center justify-center text-h1 circle-background"
-                    >{{ currentChart }}</div
-                  >
+                <v-card v-show="play" class="mx-auto rounded-circle bg-amber-lighten-4" height="140" width="140">
+                  <div class="d-flex fill-height align-center justify-center text-h1 circle-background">{{ currentChart
+                    }}</div>
                 </v-card>
               </v-expand-x-transition>
             </v-progress-circular>
@@ -100,14 +67,8 @@
                 <v-text-field v-model="answer" label="回答" class=""></v-text-field>
               </div>
               <div>
-                <v-btn
-                  prepend-icon="mdi-arrow-up-bold-box-outline"
-                  variant="tonal"
-                  block
-                  :disabled="btnDisabled"
-                  class="bg-amber-lighten-4"
-                  @click="handleAnswer"
-                >
+                <v-btn prepend-icon="mdi-arrow-up-bold-box-outline" variant="tonal" block :disabled="btnDisabled"
+                  class="bg-amber-lighten-4" @click="handleAnswer">
                   确认
                 </v-btn>
               </div>
@@ -115,20 +76,13 @@
             <v-expand-x-transition>
               <v-card v-show="gameOver" class="bg-amber-lighten-4 mx-auto" height="140" width="300">
                 <div class="feihua-h4 pt-5">体验完成</div>
-                <div class="feihua-h5 pt-5"
-                  >正确回答{{ answerList.filter((i) => i === 1).length }}题</div
-                >
+                <div class="feihua-h5 pt-5">正确回答{{ answerList.filter((i) => i === 1).length }}题</div>
               </v-card>
             </v-expand-x-transition>
           </v-col>
         </v-row>
         <v-row class="d-none">
-          <v-progress-linear
-            buffer-value="30"
-            color="orange"
-            model-value="20"
-            stream
-          ></v-progress-linear>
+          <v-progress-linear buffer-value="30" color="orange" model-value="20" stream></v-progress-linear>
         </v-row>
       </v-col>
     </v-row>
@@ -200,11 +154,11 @@ const { setloginDialogVisible } = useCounterStore()
 const play = ref(false)
 const router = useRouter()
 const completeShici = ref({
-  author: ' ',
-  content: '仙人抚我顶，结发授长生',
+  author: '-',
+  content: '仙人抚我顶，结发授长生。',
   dynasty: '元',
   id: 354527,
-  title: ' '
+  title: '-'
 })
 const colors = ['#4CAF50', '#FFEB3B', '#D50000']
 const startBtn = ref('开始')
@@ -212,20 +166,33 @@ const time = ref(100)
 const alertColor = ref(0)
 const answer = ref('')
 const currentChart = ref('')
-const currentPage = ref(1)
+const currentPage = ref(2)
 const answerList: any = ref([])
 const jielongRankRef: any = ref()
 const feiHuaMusicRef = ref()
 const btnDisabled: any = ref(true)
 //  回答正确的回合
 const gameOver: any = ref(false)
+const height: any = ref(300)
 let timer: any = null
 let fireWordEle: any = {}
+const pageDom = document.body
 
 onMounted(() => {
   feiHuaMusicRef.value.selectSong('QianNianFengYa')
   feiHuaMusicRef.value.play()
+  window.addEventListener('resize', resizeHeight)
 })
+
+const resizeHeight = (): void => {
+  if (pageDom.clientHeight > 950) {
+    height.value = 500
+  } else if (pageDom.clientHeight > 820) {
+    height.value = 400
+  }else{
+    height.value = 300
+  }
+}
 
 const gamePlay = (): void => {
   play.value = !play.value
@@ -413,7 +380,7 @@ onBeforeUnmount((): void => {
 }
 
 .feihua-subtitle-1 {
-  font-size: 1rem !important;
+  font-size: 1.6rem !important;
   font-weight: normal;
   line-height: 1.75;
   letter-spacing: 0.009375em !important;
@@ -490,6 +457,7 @@ onBeforeUnmount((): void => {
 }
 
 @keyframes clippath {
+
   0%,
   100% {
     clip-path: inset(0 0 98% 0);
